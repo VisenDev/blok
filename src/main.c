c9182a9cd41891489a80c631c651a294598c064f
Robert Burnett <dr.slate2@gmail.com>
2025-01-05 19:00:18 -0600

    basic lexer now working

--- src/main.c
+++ src/main.c
@@ -22,6 +22,11 @@ enum blok_lexer_err {
 /*parse data types*/
 struct blok_lexer {
 
+    /*diagnostics*/
+    long line;
+    long column;
+    char * error_message;
+
     /*memory*/
     char * mem;
     long mem_len;
@@ -80,7 +85,9 @@ blok_lexer_init(
         struct blok_lexer * lex,
         char * mem, long mem_len,
         char * input, long input_len
-        ) {
+) {
+    lex->error_message = NULL;
+
     lex->mem = mem;
     lex->mem_len = mem_len;
     lex->mem_i = 0;
@@ -88,6 +95,9 @@ blok_lexer_init(
     lex->input = input;
     lex->input_len = input_len;
     lex->input_i = 0;
+
+    lex->line = 0;
+    lex->column = 0;
 
     return blok_lexer_err_none;
 }
@@ -104,16 +114,25 @@ blok_lexer_get_ch(struct blok_lexer * lex) {
 }
 
 char blok_lexer_advance_ch(struct blok_lexer * lex) {
+    char ch = 0;
     lex->input_i += 1;
-    return blok_lexer_get_ch(lex);
+    ch = blok_lexer_get_ch(lex);
+
+    if(ch == '\n') {
+        lex->line += 1;
+        lex->column = 0;
+    } else {
+        lex->column += 1;
+    }
+
+    return ch;
 }
 
 enum blok_lexer_err
 blok_lexer_skip_whitespace(struct blok_lexer * lex) {
     char ch = blok_lexer_get_ch(lex);
     while(ch != 0 && blok_ch_is_whitespace(ch)) {
-        lex->input_i += 1;
-        ch = blok_lexer_get_ch(lex); 
+        ch = blok_lexer_advance_ch(lex); 
     }
     if(ch == 0) {
         return blok_lexer_err_invalid_input;
@@ -146,12 +165,13 @@ blok_lexer_get_number(struct blok_lexer * lex) {
                 is_negative = 0;
                 state = blok_lexer_number_before_decimal;
             } else {
+                lex->error_message = "Expected \'-\' or number";
                 return blok_lexer_err_invalid_input;
             }
         } else if(state == blok_lexer_number_before_decimal) {
             if(blok_ch_is_num(ch)) {
                 integer_part *= 10;
-                integer_part = blok_ch_to_num(ch);
+                integer_part += blok_ch_to_num(ch);
                 ch = blok_lexer_advance_ch(lex);
             } else if (ch == '.') {
                 ch = blok_lexer_advance_ch(lex);
@@ -162,12 +182,13 @@ blok_lexer_get_number(struct blok_lexer * lex) {
         } else if (state == blok_lexer_number_after_decimal ) {
             if(blok_ch_is_num(ch)) {
                 decimal_part *= 10;
-                decimal_part = blok_ch_to_num(ch);
+                decimal_part += blok_ch_to_num(ch);
                 ch = blok_lexer_advance_ch(lex);
             } else {
                 finished = 1;  
             }
         } else {
+            lex->error_message = "Invalid number parsing state reached";
             return blok_lexer_err_invalid_input;
         }
     }
@@ -190,6 +211,7 @@ blok_lexer_get_string(struct blok_lexer * lex) {
     if(ch == '\"') {
         ch = blok_lexer_advance_ch(lex); 
     } else {
+        lex->error_message = "Expected \"";
         return blok_lexer_err_invalid_input;
     }
 
@@ -202,11 +224,13 @@ blok_lexer_get_string(struct blok_lexer * lex) {
             } else if (ch == 'n') {
                 str[str_i] = '\n';
             } else {
+                lex->error_message = "Compiler ran out of memory";
                 return blok_lexer_err_out_of_memory;
             }
             /*update string index*/
             str_i += 1;
             if(str_i >= str_len) {
+                lex->error_message = "Compiler ran out of memory";
                 return blok_lexer_err_out_of_memory;
             }
             ch = blok_lexer_advance_ch(lex);
@@ -216,17 +240,27 @@ blok_lexer_get_string(struct blok_lexer * lex) {
                 escape = 1;
                 ch = blok_lexer_advance_ch(lex);
             } else if (ch == '\"') {
+                /*RETURN*/
                 ch = blok_lexer_advance_ch(lex);
-                /*string has ended*/
+                if(ch == '.') {
+                    lex->error_message = "Extra decimal point in number";
+                    return blok_lexer_err_invalid_input; 
+                }
+                str[str_i] = 0;
+                lex->mem_i += str_i + 1;
+                lex->token_string_lit = str;
+                lex->token_string_lit_len = str_i;
                 return blok_lexer_err_none;
             } else if(blok_ch_is_printable(ch)) {
                 str[str_i] = ch; 
                 str_i += 1;
                 if(str_i >= str_len) {
+                    lex->error_message = "Compiler ran out of memory";
                     return blok_lexer_err_out_of_memory;
                 }
                 ch = blok_lexer_advance_ch(lex);
             } else {
+                lex->error_message = "Expected printable character";
                 return blok_lexer_err_invalid_input;
             }
         }
@@ -237,12 +271,44 @@ blok_lexer_get_string(struct blok_lexer * lex) {
 
 enum blok_lexer_err
 blok_lexer_get_symbol(struct blok_lexer * lex) {
+    char ch = blok_lexer_get_ch(lex);
+    char * str = &lex->mem[lex->mem_i];
+    long str_i = 0;
+    long str_len = lex->mem_len - lex->mem_i; 
+    
+    if(blok_ch_is_alpha(ch)) {
+        str[str_i] = ch;
+        str_i += 1;
+        if(str_i >= str_len) {
+            lex->error_message = "Compiler ran out of memory";
+            return blok_lexer_err_out_of_memory;
+        }
+        ch = blok_lexer_advance_ch(lex);
+    } else {
+        lex->error_message = "Expected symbol to start with alpha";
+        return blok_lexer_err_invalid_input;
+    }
+
+    while(ch == '-' || blok_ch_is_alpha(ch) || blok_ch_is_num(ch)) {
+        str[str_i] = ch; 
+        str_i += 1;
+        if(str_i >= str_len) {
+            lex->error_message = "Compiler ran out of memory";
+            return blok_lexer_err_out_of_memory;
+        }
+        ch = blok_lexer_advance_ch(lex);
+    }
+
+    str[str_i] = 0;
+    lex->mem_i += str_i + 1;
+    lex->token_symbol_lit = str;
+    lex->token_symbol_lit_len = str_i;
 
     return blok_lexer_err_none;
 }
 
 enum blok_lexer_err
-blok_lexer_get_tok(struct blok_lexer * lex) {
+blok_lexer_get_token(struct blok_lexer * lex) {
     enum blok_lexer_err err = blok_lexer_err_none;
     char ch = 0;
 
@@ -262,27 +328,68 @@ blok_lexer_get_tok(struct blok_lexer * lex) {
 
     if(ch == '[') {
         lex->token_id = blok_token_open_bracket;
+        ch = blok_lexer_advance_ch(lex);
     } else if (ch == ']') {
         lex->token_id = blok_token_close_bracket;
+        ch = blok_lexer_advance_ch(lex);
     } else if (blok_ch_is_num(ch) || ch == '-') {
+        lex->token_id = blok_token_number_lit;
         err = blok_lexer_get_number(lex);
         if(err != blok_lexer_err_none) {
             return err;
         }
     } else if (ch == '\"') {
+        lex->token_id = blok_token_string_lit;
         err = blok_lexer_get_string(lex);
         if(err != blok_lexer_err_none) {
             return err;
         }
     } else if (blok_ch_is_alpha(ch)) {
+        lex->token_id = blok_token_symbol;
         err = blok_lexer_get_symbol(lex);
         if(err != blok_lexer_err_none) {
             return err;
         }
     } else {
+        lex->error_message = "expected [ ] string symbol or number";
         return blok_lexer_err_invalid_input;
-
     }
 
     return blok_lexer_err_none;
+}
+
+#include <stdio.h>
+#include <string.h>
+
+void blok_lexer_print_token(struct blok_lexer * lex) {
+    if(lex->token_id == blok_token_close_bracket) {
+        puts("Bracket: ]");
+    } else if (lex->token_id == blok_token_open_bracket) {
+        puts("Bracket: [");
+    } else if (lex->token_id == blok_token_symbol) {
+        printf("Symbol: %s\n", lex->token_symbol_lit);
+    } else if (lex->token_id == blok_token_string_lit) {
+        printf("String: %s\n", lex->token_string_lit);
+    } else if (lex->token_id == blok_token_number_lit) {
+        printf("Number: %ld.%ld\n", lex->token_integer_lit, lex->token_decimal_lit);
+    }
+}
+
+
+
+int main() {
+    #define len 100000
+    #define input "[asdf [ 1.123 \"hello world\" ][] 12.3.3 ]"
+    struct blok_lexer lex;
+    enum blok_lexer_err err; 
+    char mem[len];
+
+    err = blok_lexer_init(&lex, mem, len, input, strlen(input));
+    err = blok_lexer_get_token(&lex);
+    while(err == blok_lexer_err_none) {
+        blok_lexer_print_token(&lex);
+        err = blok_lexer_get_token(&lex);
+    }
+
+    return 0;
 }
