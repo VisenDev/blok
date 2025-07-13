bd17b983b684ea6fcc593c82559adae678f8474e
Robert Burnett <dr.slate2@gmail.com>
2025-07-13 17:49:31 -0500

    work on profiler

--- src/blok_profiler.c
+++ src/blok_profiler.c
@@ -1,8 +1,33 @@
+#ifndef BLOK_PROFILER_C
+#define BLOK_PROFILER_C
 
-/*
- * TODO
- * implement profiler using chromium trace format
- */
-typedef struct {
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdbool.h>
 
-} blok_Profiler;
+static FILE * blok_profiler_output_file = NULL;
+static bool blok_profiler_prepend_comma = false;
+
+uint64_t blok_profiler_timestamp(void) {
+    struct timeval currentTime = {0};
+	gettimeofday(&currentTime, NULL);
+	return currentTime.tv_sec * 1000000 + currentTime.tv_usec;
+}
+
+static inline int blok_profile_interal(FILE * output, const char * name, bool active) {
+    static bool first = true;
+    if(first) {
+        first = false;
+    } else {
+        fprintf(output, ",\n");
+    }
+    fprintf(output, "{ \"name\": %s, \"ph\": \"%s\", \"ts\": %llu }", name, active ? "B" : "E", blok_profiler_timestamp());
+    return 0;
+}
+
+#define blok_profile(name) \
+    for(int i = blok_profile_internal(; i < 1; ++i) 
+
+#endif /*BLOK_PROFILER_C*/
+
--- src/main.c
+++ src/main.c
@@ -3,13 +3,28 @@
 #include "blok_obj.c"
 #include "blok_reader.c"
 #include "blok_evaluator.c"
+#include "blok_profiler.c"
 
 int main(void) {
+    FILE * profile = fopen("main.dat", "w");
 
+    fprintf(profile, "[");
+    blok_profile(profile, "arena tests", true);
     blok_arena_run_tests();
+    blok_profile(profile, "arena tests", false);
+
+    blok_profile(profile, "slice tests", true);
     blok_slice_run_tests();
+    blok_profile(profile, "slice tests", false);
+
+    blok_profile(profile, "vec tests", false);
     blok_vec_run_tests();
+    blok_profile(profile, "vec tests", false);
+
+    blok_profile(profile, "vec tests", false);
     blok_table_run_tests();
+    blok_profile(profile, "vec tests", false);
+    fprintf(profile, "]");
 
     blok_Arena a = {0};
     blok_State s = {0};
@@ -22,6 +37,7 @@ int main(void) {
     blok_primitive_toplevel(&s, &a, blok_list_from_obj(source), output);
     fclose(output);
 
+    fclose(profile);
     blok_arena_free(&a);
     return 0;
 }
